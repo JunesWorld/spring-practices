d8009bb62ac0523c0bbb67049a9a1235c0a58818
June <sungj1207@gmail.com>
2023-04-19 15:10:56 +0900

    helloweb #3

--- helloweb/src/main/java/com/junesworld/helloweb/controller/HelloController.java
+++ helloweb/src/main/java/com/junesworld/helloweb/controller/HelloController.java
@@ -5,10 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class HelloController {
+	public void a() {
+		
+	}
 
 	// Tomcat Servlet에서 실행 
 	@RequestMapping("/hello")
 	public String hello() {
+		return "/WEB-INF/views/hello.jsp"; 
+	}
+	
+	@RequestMapping("/hello2")
+	// localhost8080:helloweb/hello2?name=june&no=10 -> [server] name : june, no : 10
+	public String hello(String name, Long no) {
+		System.out.println("name:" + name + ", no:" + no); 
 		return "/WEB-INF/views/hello.jsp"; 
 	}
 }
