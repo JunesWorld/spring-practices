ec459eee785fdc8a25a7e56fbfab987a30ca9365
June <sungj1207@gmail.com>
2023-04-20 18:39:25 +0900

    Emaillist #2

---
+++ emaillist/src/main/java/com/junesworld/emaillist/controller/EmaillistController.java
@@ -0,0 +1,37 @@
+package com.junesworld.emaillist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.junesworld.emaillist.repository.EmaillistRepository;
+import com.junesworld.emaillist.vo.EmaillistVo;
+
+@Controller
+public class EmaillistController {
+	@Autowired  // Root EmailRepo -> Web EmailRepo 자동 DI!!
+	private EmaillistRepository emaillistRepository;
+	
+	// @ResponseBody  // test 위해 나에게 받기
+	@RequestMapping("")
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "/WEB-INF/views/add.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
+	}
+}
