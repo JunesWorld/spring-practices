415cc2954456522e8ace1460c45a3a7dd305006b
June <sungj1207@gmail.com>
2023-05-05 23:34:00 +0900

    fileupload #3

--- fileupload/src/main/java/com/junesworld/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/junesworld/fileupload/service/FileUploadService.java
@@ -1,5 +1,7 @@
 package com.junesworld.fileupload.service;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,13 +18,31 @@ public class FileUploadService {
 		String originalFilename = multipartFile.getOriginalFilename();
 		
 		String extName = originalFilename.substring(originalFilename.lastIndexOf('.')+1);
+		String restoreFilename = generateSaveFilename(extName);
 		
 		Long fileSize = multipartFile.getSize();
 		
 		System.out.println("###### + originalFilename");
+		System.out.println("###### + restoreFilename");
 		System.out.println("###### + fileSize");
 		
 		return url;
+	}
+
+	private String generateSaveFilename(String extName) {
+		String filename = "";
+		
+		Calendar calendar = Calendar.getInstance();
+		filename += calendar.get(Calendar.YEAR);
+		filename += calendar.get(Calendar.MONTH);
+		filename += calendar.get(Calendar.DATE);
+		filename += calendar.get(Calendar.HOUR);
+		filename += calendar.get(Calendar.MINUTE);
+		filename += calendar.get(Calendar.SECOND);
+		filename += calendar.get(Calendar.MILLISECOND);
+		filename += ("." + extName);
+		
+		return filename;
 	}
 
 }
